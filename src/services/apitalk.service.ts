69b3da9d05ad4bfd7a4448580a020ce658f3a520
Ismail Can Guner <ismail.guner@kolayik.com>
2019-07-25 10:44:47 +0300

    Edit api service

--- src/services/apitalk.service.ts
+++ src/services/apitalk.service.ts
@@ -6,7 +6,7 @@ import { Observable } from 'rxjs';
 })
 export class ApitalkService {
     private apiKey = 'a9cec370345e41ae9e8f42141a60d727';
-    private apiUrl = 'https://api.weatherbit.io/v2.0/current?';
+    private apiUrl = 'https://cors-anywhere.herokuapp.com/https://api.weatherbit.io/v2.0/current?';
     private lang = 'tr';
     constructor(private http: HttpClient) {}
     getWeatherByGeo(lat, lon): Observable<any> {
